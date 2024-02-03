94687507525e6311d0318f55cd84f0be96e44582
Mounir <polomnr1@gmail.com>
2024-02-04 00:41:56 +0100

    get logged user in server

---
+++ src/lib/payload-utils.ts
@@ -0,0 +1,23 @@
+import { User } from "@/payload-types";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { NextRequest } from "next/server";
+
+export const getServerSideUser = async (
+  cookies: NextRequest["cookies"] | ReadonlyRequestCookies,
+) => {
+  const token = cookies.get("token")?.value;
+  if (!token) return null;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  if (!response.ok) return null;
+
+  const { user } = (await response.json()) as { user: User };
+
+  return { user };
+};
